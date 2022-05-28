b478aaa3b4a44e7a1df96296f0a562df19c4f2ea
johngartsu <john.gartsu@gmail.com>
2022-05-27 22:02:58 -0400

    added db, schema, and models

---
+++ database.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from .config import get_settings
+
+# db entry point
+engine = create_engine(
+    get_settings().db_url, connect_args={"check_same_thread": False}
+)
+
+# create working db session
+SessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine
+)
+
+# connects to db engine to the SQLAlchemy functionality of models
+Base = declarative_base()
--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-from http.client import HTTPException
-from fastapi import FastAPI
+import secrets
+from fastapi import Depends, FastAPI, HTTPException
 import validators
-
-from . import schemas
+from sqlalchemy.orm import Session
+from .database import SessionLocal, engine
+# from . import schemas, models
+import schemas
+import models
 # instantiate FastAPI class
 app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    except:
+        return f'Error occurred get_db fn'
+    finally:
+        db.close()
 
 # create exception handling for 400 
 def raise_bad_request(msg):
@@ -21,8 +34,25 @@ def read_root():
     return 'This is url shortner'
 
 # POST METHOD
-@app.post("/url")
-def create_url(url: schemas.URLBase):
-    if not validators.url(url.target_url)
-        raise_bad_request(msg="### URL not valid")
-    return f'TODO: Create DB Entry for: {url.target_url}'
+@app.post("/url", response_model=schemas.URLInfo)
+def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
+    try:
+        if not validators.url(url.target_url):
+            raise_bad_request(msg="### URL not valid")
+        
+        chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        key = "".join(secrets.choice(chars) for _ in range(5))
+        secret_key = "".join(secrets.choice(chars) for _ in range(8))
+        db_url = models.URL(
+            target_url = url.target_url, key=key, secret_key=secret_key
+        )
+        db.add(db_url)
+        db.commit()
+        db.refresh(db_url)
+        db_url.url = key
+        db_url.admin_url = secret_key
+
+        return db_url
+    except:
+        return f'### Error: create_url fn error in post request...'
+    # return f'TODO: Create DB Entry for: {url.target_url}'
